72f5642242408bdd7a11b17c236a46d4bda1a96b
turowski-rex <153832445+turowski-rex@users.noreply.github.com>
2025-03-27 23:05:28 +0100

    Notification sounds
    Added notification sound and muting/unmuting functionality.

--- Client.py
+++ Client.py
@@ -1,10 +1,13 @@
 import socket
 import threading
+import winsound
+
 
 class chatClient:
     def __init__(self, host='127.0.0.1', port=8080): # init client with host and port of chatServer
         self.host = host
         self.port = port
+        self.notification = True #by default
 
         self.connectionSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # TCP socket
         self.connectionSocket.connect((self.host, self.port))  # cnnect to server
@@ -18,11 +21,21 @@ class chatClient:
         # continuously send messages to server
         while True:
             message = input()  #user input
-            try:
-                self.connectionSocket.send(message.encode())  # send message to server
-            except Exception as e:
-                print(f"Error sending message: {e}") # error handling
-                break
+            
+            if message == "#mute":
+                self.notification = False
+                print("Muted notifications.")
+
+            elif message == "#unmute":
+                self.notification = True
+                print("Unmuted notifications.")
+
+            else:
+                try:
+                    self.connectionSocket.send(message.encode())  # send message to server
+                except Exception as e:
+                    print(f"Error sending message: {e}") # error handling
+                    break
 
     def receiveMessages(self):
         # Continuously receive messages from the server
@@ -32,9 +45,20 @@ class chatClient:
                 if not message:
                     break  # If the message is empty, the server has disconnected
                 print(message)  # Print the received message
+
+                if self.notification:
+                    self.playNotification()
+
             except Exception as e:
-                print(f"Error receiving message: {e}")  # Handle any errors
+                print(f"Error receiving message: {e}")  # error handling
                 break
+        
+    def playNotification(self):
+        # Ref: https://www.geeksforgeeks.org/python-winsound-module/
+        # Using winsound instead of playsound, because no additional file and path needed
+        winsound.PlaySound("SystemExit", winsound.SND_ALIAS)
+        
+
 
 if __name__ == "__main__":
     client = chatClient()  # make client instance
